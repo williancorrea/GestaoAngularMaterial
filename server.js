10abe8d91b9ee784efea453c5e3ae2a8e48c856c
Willian Vagner Vicente Correa <willian.vag@gmail.com>
2019-10-18 21:32:39 -0300

    Adicionando Server Node

---
+++ server.js
@@ -0,0 +1,19 @@
+const compression = require('compression');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4200;
+
+// Gzip
+app.use(compression());
+
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(port);
+
+console.log(`Servidor esta escutando na porta ${port}`);
